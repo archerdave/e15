608e01dfaefee3f714b0041d7ddb34286f802890
David Harvill <dharvill@gmail.com>
2022-02-08 21:01:19 +0000

    Added author info to to README; initialized practice app.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # e15
 Coursework for CSCI E-15, Spring 2022
+By: David Harvill
---
+++ practice/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href=data, rel=icon>
+    <title>Practice App</title>
+</head>
+
+<body>
+    <h1>Practice App</h1>
+</body>
+
+</html>
