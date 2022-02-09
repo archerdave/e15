f28378bc10cd0472db87cafc7d106b35f9b786a5
David Harvill <dharvill@gmail.com>
2022-02-09 03:23:21 +0000

    Basic functionality for vowelCount is present.

--- p1/index.php
+++ p1/index.php
@@ -17,14 +17,31 @@ $vowelCountTestStrings = [
     "Bonzai?",
 ];
 
+
 function isPalindrome(string $string){
     #stub
     return false;
 }
 
 function vowelCount(string $string){
-    #stub
-    return -1;
+    //for now, testing with unaccented, non-special vowels.
+    //testing will be done by a case-insenstive comparison against
+    //the letter in question
+    $vowels = ['a', 'e', 'i', 'o', 'u'];
+    $length = strlen($string);
+    $numberOfVowels = 0;
+
+    /*  By definition in the PHP documentation 
+        (https://www.php.net/manual/en/language.types.string.php#language.types.string.details), 
+        strings are arrays of characters. Step through the string array, searching for each 
+        character in the string in a defined array of vowels.
+     */
+    for($i = 0; $i < $length; $i++){
+        if(in_array(strtolower($string[$i]),$vowels,false)){
+            $numberOfVowels++;
+        }
+    }
+    return $numberOfVowels;
 }
 
 require 'index-view.php';
