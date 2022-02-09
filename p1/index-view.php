cc3bc8eeecf45bbb757579a98524810a05215994
David Harvill <dharvill@gmail.com>
2022-02-09 02:47:11 +0000

    Initial P1 skeleton

--- p1/index-view.php
+++ p1/index-view.php
@@ -9,6 +9,21 @@
 
 <body>
     <h1>Project 1</h1>
+    <h2>First up... palindrome testing!</h2>
+    <?php
+    foreach ($palindromeTestStrings as $testString) {
+        $result = isPalindrome($testString) ? "Yes" : "No";
+        echo "Is ".$testString." a palindrome? ".$result."<br>";
+    }?>
+
+    <h2>And next... how many vowels are there?</h2>
+    <?php
+    foreach ($vowelCountTestStrings as $testString) {
+        $result = vowelCount($testString);
+        echo $testString." contains ".$result." vowels<br>";
+    }?>
+
+
 </body>
 
 </html>
--- p1/index.php
+++ p1/index.php
@@ -1,3 +1,30 @@
 <?php
 
+
+$palindromeTestStrings = [
+    "madam",
+    "Madam",
+    "adam",
+    "race car",
+    "Anna!",
+];
+
+$vowelCountTestStrings = [
+    "aeiou",
+    "AEIOU",
+    "Hello World",
+    "dry",
+    "Bonzai?",
+];
+
+function isPalindrome(string $string){
+    #stub
+    return false;
+}
+
+function vowelCount(string $string){
+    #stub
+    return -1;
+}
+
 require 'index-view.php';
