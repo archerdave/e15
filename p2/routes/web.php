6a1ad2f5c227cf6aaeb5f004c949838f94ffe6d7
David Harvill <dharvill@gmail.com>
2022-03-21 19:58:24 +0000

    reinitialized README, changed default route to demonstrate correct operation

--- p2/routes/web.php
+++ p2/routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return '<h1>And here ye shall find Project 2...</h1>';
 });
