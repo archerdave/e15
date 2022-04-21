9fb2ded3a886dcd94b998984c872bd2758d2c92e
David Harvill <dharvill@gmail.com>
2022-04-21 20:13:54 +0000

    Added rounds migration

---
+++ p3/database/migrations/2022_04_21_201123_create_rounds_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rounds', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('twenty')->constrained('scores');
+            $table->foreignId('thirty')->constrained('scores');
+            $table->foreignId('forty')->constrained('scores');
+            $table->foreignId('twentyTimed')->constrained('scores');
+            $table->foreignId('thirtyTimed')->constrained('scores');
+            $table->foreignId('fortyTimed')->constrained('scores');
+            $table->foreignId('person_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rounds');
+    }
+};
