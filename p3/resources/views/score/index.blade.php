c9fd1b4c3ee7018b216466818305d45ea5377c8e
David Harvill <dharvill@gmail.com>
2022-05-11 03:49:31 +0000

    modification of score index layout

--- p3/resources/views/score/index.blade.php
+++ p3/resources/views/score/index.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts/master')
 @section('main')
-@if( $scores->count() == 0 )
-<p>I'm sorry, but you don't have any scores recorded.  Maybe you should go and practice?</p>
-@else
 <h2>Enter a new score</h2>
 <table class='scoreTable' id='newScoreTable'>
     <tr>
@@ -26,23 +23,26 @@
     @include('score/errorRow')
 </table>
 <h2>You have the following scores on record</h2>
-<table class='scoreTable' id='scoresTable'>
-    <tr>
-        <th></th>
-        <th>Date</th>
-        <th>Distance</th>
-        <th>Timed</th>
-        <th>Points</th>
-    </tr>
-    @foreach($scores as $score)
-    <tr class={{($score->id == session('changedId')) ?  'changed' : ''}}>
-        <td><a href='/scores/{{$score->id}}/edit'>edit</a></td>
-        <td>{{$score->date}}</td>
-        <td>{{$score->distance}}</td>
-        <td><input type='checkbox' {{$score->isTimed ? 'checked' : ''}}></td>
-        <td>{{$score->points}}</td>
-    </tr>
-    @endforeach
-</table>
+@if( $scores->count() == 0 )
+    <p>I'm sorry, but you don't have any scores recorded.  Maybe you should go and practice?</p>
+@else
+    <table class='scoreTable' id='scoresTable'>
+        <tr>
+            <th></th>
+            <th>Date</th>
+            <th>Distance</th>
+            <th>Timed</th>
+            <th>Points</th>
+        </tr>
+        @foreach($scores as $score)
+        <tr class={{($score->id == session('changedId')) ?  'changed' : ''}}>
+            <td><a href='/scores/{{$score->id}}/edit'>edit</a></td>
+            <td>{{$score->date}}</td>
+            <td>{{$score->distance}}</td>
+            <td><input type='checkbox' {{$score->isTimed ? 'checked' : ''}}></td>
+            <td>{{$score->points}}</td>
+        </tr>
+        @endforeach
+    </table>
 @endif
 @endsection
